8d7f68a8c67f60963d01c18123c8837186a104c5
somcpp <saubhagya.sharma3.14@gmail.com>
2026-08-16 18:52:05 +0530

    feat: implement members management modal to handle role updates and member kicks

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -62,7 +62,7 @@ export function ServerHeader({server, role}: ServerHeaderProps) {
         )}
         {isAdmin && (
           <DropdownMenuItem
-            // onClick={() => onOpen("members", { server })}
+            onClick={() => onOpen("members", { server })}
             className="px-3 py-2 text-sm cursor-pointer"
           >
             Manage Members
