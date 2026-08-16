466daa810ecc464505c8b51b0aa5d8475640464c
somcpp <saubhagya.sharma3.14@gmail.com>
2026-08-16 19:15:09 +0530

    feat: add API route to update member roles within a server

---
+++ app/api/members/[memberId]/route.ts
@@ -0,0 +1,68 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  {params} : {params : Promise<{memberId : string}>}
+) {
+  try {
+    const profile = await currentProfile();
+    if(!profile){
+      return new NextResponse("Unauthorized", { status: 401});
+    }
+
+    const {memberId} = await params;
+
+    if(!memberId){
+      return new NextResponse("Member ID Missing", { status: 400});
+    }
+
+    const {searchParams} = new URL(req.url);
+    const serverId = searchParams.get("serverId");
+
+    if(!serverId){
+      return new NextResponse("Server ID Missing", { status: 400});
+    }
+
+    const {role} = await req.json();
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        profileId: profile.id,
+      },
+      data: {
+        members: {
+          update: {
+            where: {
+              id: memberId,
+              profileId: {
+                not: profile.id
+              }
+            },
+            data: {
+              role
+            }
+          }
+        }
+      },
+      include: {
+        members: {
+          include: {
+            profile: true
+          },
+          orderBy: {
+            role: "asc"
+          }
+        },  
+      }
+    })
+
+    return NextResponse.json(server);
+
+  } catch (error) {
+    console.log("[MEMBER_ID]_PATCH", error);
+    return new NextResponse("Internal Server Error", { status: 500});
+  }
+}
+
