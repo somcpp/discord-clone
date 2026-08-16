326690a66a9a26d549074432ae2304d6aae346f4
somcpp <saubhagya.sharma3.14@gmail.com>
2026-08-16 16:37:32 +0530

    feat: implement server invite code generation and fix: server header dropdown UI

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -36,10 +36,10 @@ export function ServerHeader({server, role}: ServerHeaderProps) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="focus:outline-none" >
-        <button className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+        <div className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
           {server.name}
           <ChevronDown className="h-5 w-5 ml-auto" />
-        </button>
+        </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
         {isModerator && (
