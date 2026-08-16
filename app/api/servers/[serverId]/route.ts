2684d0114bb95e3a3cf8cccd47c49af64a1731af
somcpp <saubhagya.sharma3.14@gmail.com>
2026-08-17 01:23:30 +0530

    feat: create Delete api for server deletion

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -26,3 +26,23 @@ export async function PATCH(req:Request, {params}: {params: Promise<{serverId: s
     return new NextResponse("Internal Error", {status: 500});
   }
 }
+
+export async function DELETE(req:Request, {params}: {params: Promise<{serverId: string}>}) {
+  try {
+    const profile = await currentProfile();
+    const {serverId} = await params;
+    if(!profile) {
+      return new NextResponse("Unauthorized", {status: 401})
+    }
+    const server = await db.server.delete({
+      where:{
+        id: serverId,
+        profileId: profile.id,
+      },
+    })
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_DELETE]", error);
+    return new NextResponse("Internal Error", {status: 500});
+  }
+}
